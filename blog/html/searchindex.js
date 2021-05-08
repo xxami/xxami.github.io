83046d31a85ac3c121c5786a23fc9acf41d5cc9d
lilah <lilah.willacott@gmail.com>
2021-05-08 19:53:28 +0100

    nuke old junk

--- blog/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:50,filenames:["2016/08/12/hello-world","master","pages/about"],objects:{},objnames:{},objtypes:{},terms:{about:1},titles:["Hello World!","Sitemap","About"],titleterms:{about:2,hello:0,sitemap:1,world:0}})
--- readme.md
+++
@@ -1,1 +0,0 @@
-# xxami.github.io
